f613b94985592411089fa3a1f5e86b28e212615a
bh4sk4r <110119179+bh4sk4r@users.noreply.github.com>
2023-06-10 17:30:39 +0000

    Before 10th June

--- stack/push_bottom_recursive.cpp
+++ stack/push_bottom_recursive.cpp
@@ -7,21 +7,17 @@
 #define endl "\n"
 
 using namespace std;
-void PushBottom(int x,stack<int> &s)
+void PushBottom(int x,stack<int>& s)
 {
-    stack<int> temp;
-    int n = s.size();
-    while(!s.empty())
+    if(s.empty())
     {
-        temp.push(s.top());
-        s.pop();
+        s.push(x);
+        return;
     }
-    s.push(x);
-    while(!temp.empty())
-    {
-        s.push(temp.top());
-        temp.pop();
-    }
+    int y = s.top();
+    s.pop();
+    PushBottom(x,s);
+    s.push(y);
 }
 int main()
 {
@@ -29,14 +25,15 @@ int main()
     int n;
     cin>>n;
     stack<int> s;
-    int temp;
     for(int i=0;i<n;i++)
     {
+        int temp;
         cin>>temp;
         s.push(temp);
     }
-    cin>>temp;
-    PushBottom(temp,s);
+    int x;
+    cin>>x;
+    PushBottom(x,s);
     while(!s.empty())
     {
         cout<<s.top()<<" ";
